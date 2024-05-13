feefbb52ce0585998e1f4573b1b7d6cb214c13b6
Ian Curl <iancurl.salvador21@gmail.com>
2024-05-14 01:56:36 +0800

    tapos na yung portfolio

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,1 +1,16 @@
-console.log("Hello Gumagana yung javascript mo idol");
+// console.log("Hello Gumagana yung javascript mo idol");
+
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("active-tab");
+
+function openTab(tabName, event) {
+    for(tabLink of tabLinks) {
+        tabLink.classList.remove("active-link");
+    }
+    for(tabContent of tabContents) {
+        tabContent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
+}
